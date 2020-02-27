b51d2d27c5c6d49711c03dbf0d7dd0b970896e75
keneikevin <kevinkenei9@gmail.com>
2020-02-27 06:53:37 +0300

    adding recyclerview

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,15 +16,37 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
 // TODO (02) Create SleepNightAdapter class and extend it
 // from RecyclerView.Adapter<TextItemViewHolder>
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
 
-// TODO (03) Create a variable, data, that holds a list of SleepNight.
+var data = listOf<SleepNight>()
+    set(value) {
+        field = value
+        notifyDataSetChanged()
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+val layoutInflater = LayoutInflater.from(parent.context)
+        val view =layoutInflater.inflate(R.layout.text_item_view,parent,false) as TextView
+        return TextItemViewHolder(view)
+    }
 
-// TODO (04) Override getItemCount() to return the total number of items in the data set.
+    override fun getItemCount()= data.size
 
-// TODO (05) Override onBindViewHolder() and have it update the contents of the
-// ViewHolder to reflect the item at the given position.
-
-// TODO (06) Override onCreateViewHolder(). We'll complete this method
-// in a later exercise.
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    val item = data[position]
+        holder.textView.text = item.sleepQuality.toString()
+        if(item.sleepQuality <= 1){
+            holder.textView.setTextColor(Color.RED)
+        }else{holder.textView.setTextColor(Color.BLACK)}
+    }
+    }
